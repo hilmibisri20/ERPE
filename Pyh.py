69802cfaf1c193224dcc7afbdf4892596cd023b2
HilmiBisri20 <113221213+hilmibisri20@users.noreply.github.com>
2022-09-10 10:23:34 +0700

    Add files via upload

---
+++ Pyh.py
@@ -0,0 +1,69 @@
+
+#def my_f():
+ #   print("my_fa")
+ #   
+#print(my_f())
+# print("Pa")
+
+
+# def func_py():
+    # print("PA")
+# func_py()
+# float
+# string
+# boolean
+nama = "PO" #string
+umur = 369 #integer
+float(umur) #float
+Tinggi_Badan = 180.5 #float 
+Absensi = True #Boolean
+Alfa = False #Boolean
+Makanan = ["Kebab", "Sayur sop", "Nasi", "Dll"] #list (Ordered, Mutable, Changeable, Not Unique)
+Makanan.append("SUP KELELAWAR")
+
+Makanan.remove ("Kebab")
+print(Makanan)
+Makanan2 = ("Sapi", "Vyroz", 21, 34) #tuple (Ordered, Not Mutable, Not changeable, Not Unique)
+print(Makanan2)
+
+Makanan[2] = "Kelelawar"
+print(Makanan2)
+Jumlah = {1, 2, 3, 4, 5, 6} #set (Not Ordered, Not Mutable, Changeable, Unique)
+Jumlah.add("Sop")
+print("PA " + nama, "umur", umur) #String + integer
+print("Umur " + str(umur)) #string + string
+print("Tinggi nya ", Tinggi_Badan) #float
+print(Tinggi_Badan, "Tingginya ", type(Tinggi_Badan)) #float
+print(Absensi) #boolean
+print(Alfa) #Boolean
+#if(Absensi = false):
+ #   print("Alfa")
+#if(Alfa = true):
+#    print("Absensi")
+print(Makanan[0]) #awal dari kiri
+print(Makanan[-1]) #awal dari kanan
+print(Makanan[2]) #list
+#print(Makanan2(2)) #tuple
+#print(Jumlah(2)) #set
+#AbstractHTTPHandler((http))
+#HTTPSHandler((http))
+
+# Aritmatika coy
+a = 10
+b = 69
+c = 50
+input
+print("tambah", a + b + c)
+print("kurang " + str(a - b - c))
+print("Pembagian", a / b / c)
+print("Kali", a * b * c)
+print("Pembagian 2 ", b // c)
+print("mODULUS", 60 % 10 )
+
+nama = input("Insert your name: ")
+umur = int(input("Insert your age: "))
+
+print(nama)
+print(umur)
+#menghitung konversi suhu Celcius > farenheit
+#menghitung konversi suhu farenheit > reamur
